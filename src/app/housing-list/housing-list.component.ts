7dd9424dad9939632ef8a445bbb1e8cfb284cd17
Anthony Mogotlane <almogotlane@gmail.com>
2023-04-07 11:18:52 +0200

    Added members to HousingLocationComponent class

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-list',
   templateUrl: './housing-list.component.html',
   styleUrls: ['./housing-list.component.css']
 })
+
 export class HousingListComponent {
 
+  @Input() locationList: HousingLocation[] = [];
+  results: HousingLocation[] = [];
+
+  searchHousingLocations(searchText: string) {
+    if(!searchText) return;
+    this.results = this.locationList.filter((location: HousingLocation) => location.city.toLowerCase().includes(searchText.toLowerCase()))
+    console.log(this.results);
+  }
 }
